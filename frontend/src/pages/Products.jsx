7c864bd587615c705a89a910d9c7c50f35628ca6
Suhana Pendhari <suhanapendhari786@gmail.com>
2025-12-04 00:42:48 +0530

    fix-bug

--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import '../pageStyles/products.css';
+import '../pageStyles/Products.css';
 import PageTitle from '../components/PageTitle';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
